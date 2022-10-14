b6d29f11c7e096ea5272d1c2fe8812dd93c83cb4
tariq19ansari <tariq19ansari@gmail.com>
2022-10-14 16:14:01 -0700

    Performing basic functions on Iris data set

---
+++ Iris Data set.R
@@ -0,0 +1,18 @@
+data(iris)
+dim(iris)
+# Another way to get the dim is to use ncol or nrow:
+ncol(iris)
+nrow(iris)
+# Variable names or column names
+names(iris)
+colnames(iris)
+
+# Structure of the dataframe, note that the difference between num and Factor
+str(iris)
+
+#Subsetting the dataset I: Numerical Index and Variable Names
+#Get the first 5 rows/obs
+iris[1:5, ]
+
+#Get Sepal.Length of the first 10 rows
+iris[1:10, "Sepal.Length"]
