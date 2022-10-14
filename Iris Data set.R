32c4e7aa1002840d740f42c1e81a357dd0a840e3
tariq19ansari <tariq19ansari@gmail.com>
2022-10-14 16:42:37 -0700

    Used SQL statement and subset logical condition

--- Iris Data set.R
+++ Iris Data set.R
@@ -23,3 +23,30 @@ iris$Sepal.Length[1:10]
 # or we can use the column index instead of variable names. Since Sepal.Length is the first
 #column:
 iris[1:10, 1]
+
+################# Subsetting the dataset II: Logical Conditions ###############
+
+## There are 3 ways to get subset of data that satisfy certain logical conditions. These kind of
+##operations are called filtering in Excel.
+
+## Suppose we want to get the subset of data that has Sepal.Length > 5 and Sepal.Width >
+#4.
+# Use subset function
+subset(x = iris, subset = Sepal.Length > 5 & Sepal.Width > 4)
+
+## Alternative way -- You can omit the x = and sebset = part
+subset(iris, Sepal.Length > 5 & Sepal.Width > 4)
+
+
+## Use logical vectors
+iris[which(iris$Sepal.Length > 5 & iris$Sepal.Width > 4), ]
+
+
+##  Use SQL statement
+install.packages("sqldf")
+library(sqldf)
+sqldf('select * from iris where "Sepal.Length">5 and "Sepal.Width">4')
+
+
+
+
